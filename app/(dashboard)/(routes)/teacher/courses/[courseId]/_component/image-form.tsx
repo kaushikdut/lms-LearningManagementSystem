ba827c2f0531696ef0b9b75522bfc9203598c6c7
Vicky Dutta <duttakoushik779@gmail.com>
2024-08-25 16:14:03 +0530

    feat: implemented uploadthing

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/image-form.tsx
@@ -10,7 +10,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
