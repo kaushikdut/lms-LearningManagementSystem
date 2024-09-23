1943b35d036cc91fc36b4126a389dc51caea3f5b
Vicky Dutta <duttakoushik779@gmail.com>
2024-09-23 20:50:51 +0530

    feat: add course sidebar, sidebar-items and chapter locked functionality

---
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -0,0 +1,47 @@
+import { getProgress } from "@/actions/get-progress";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import CourseSidebar from "./_components/course-sidebar";
+
+const CourseLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { courseId: string };
+}) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: { id: params.courseId },
+    include: {
+      chapters: {
+        where: { isPublished: true },
+        include: { userProgress: { where: { userId } } },
+        orderBy: { position: "asc" },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const progressCount = await getProgress(userId, course.id);
+
+  return (
+    <div className="h-full">
+      <div className="fixed inset-y-0 z-50 hidden h-full w-80 flex-col md:flex">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+
+      <main className="h-full pt-20 md:pl-80">{children}</main>
+    </div>
+  );
+};
+
+export default CourseLayout;
